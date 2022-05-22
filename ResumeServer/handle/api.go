1f8cbf2948d294d76a5bc1e3f261501dd2fea2b1
zz001357 <407102799@qq.com>
2022-05-23 00:23:18 +0800

    1

--- ResumeServer/handle/api.go
+++ ResumeServer/handle/api.go
@@ -66,3 +66,62 @@ func (s *Server) GetResumeInfo(ctx context.Context, in *pb.ResumeInfoRequest) (*
 		return &pb.ResumeInfoReply{Name: "获取基本信息", Message: "ok", Data: d}, nil
 	}
 }
+func (s *Server) GetResumeInfo1(ctx context.Context, in *pb.ResumeInfoRequest) (*pb.ResumeInfoReply, error) {
+	/**
+	 * @Name 获取简历信息
+	 * @Param	search_key	string	模糊搜索关键字
+	 * @Return
+	 * @Date 2022/5/10 15:01
+	 * @Author ZhangZe
+	 **/
+	searchKey := in.SearchKey
+	var whereSql string
+	if searchKey != "" {
+		whereSql = fmt.Sprint(" where  v_position like '%", searchKey, "%' or v_word_content like '%", searchKey, "%'")
+	} else {
+		whereSql = ""
+	}
+	sql := fmt.Sprint("select * from t_resume", whereSql)
+	_, data, err := ReadSql(sql, Connection())
+	var d []*pb.OutResumeInfo
+	for _, val := range data {
+		out := pb.OutResumeInfo{Id: val["id"], VBaseId: val["v_base_id"], VStartDate: val["v_start_date"],
+			VEndDate: val["v_end_date"], VWordContent: val["v_word_content"], VPosition: val["v_position"]}
+		d = append(d, &out)
+	}
+	if err != nil {
+		return &pb.ResumeInfoReply{Name: "获取基本信息", Message: err.Error(), Data: nil}, nil
+	} else {
+		return &pb.ResumeInfoReply{Name: "获取基本信息", Message: "ok", Data: d}, nil
+	}
+}
+
+func (s *Server) GetResumeInfo2(ctx context.Context, in *pb.ResumeInfoRequest) (*pb.ResumeInfoReply, error) {
+	/**
+	 * @Name 获取简历信息
+	 * @Param	search_key	string	模糊搜索关键字
+	 * @Return
+	 * @Date 2022/5/10 15:01
+	 * @Author ZhangZe
+	 **/
+	searchKey := in.SearchKey
+	var whereSql string
+	if searchKey != "" {
+		whereSql = fmt.Sprint(" where  v_position like '%", searchKey, "%' or v_word_content like '%", searchKey, "%'")
+	} else {
+		whereSql = ""
+	}
+	sql := fmt.Sprint("select * from t_resume", whereSql)
+	_, data, err := ReadSql(sql, Connection())
+	var d []*pb.OutResumeInfo
+	for _, val := range data {
+		out := pb.OutResumeInfo{Id: val["id"], VBaseId: val["v_base_id"], VStartDate: val["v_start_date"],
+			VEndDate: val["v_end_date"], VWordContent: val["v_word_content"], VPosition: val["v_position"]}
+		d = append(d, &out)
+	}
+	if err != nil {
+		return &pb.ResumeInfoReply{Name: "获取基本信息", Message: err.Error(), Data: nil}, nil
+	} else {
+		return &pb.ResumeInfoReply{Name: "获取基本信息", Message: "ok", Data: d}, nil
+	}
+}
