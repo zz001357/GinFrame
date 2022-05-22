522d3af2a8836605f6b2a67c2c131408f8971e05
zz001357 <407102799@qq.com>
2022-05-22 15:52:31 +0800

    1

---
+++ ResumeServer/handle/config.go
@@ -0,0 +1,23 @@
+/**
+* @Author: ZhangZe
+* @Date: 2022/4/24 19:28
+ */
+
+package handle
+
+import (
+	"github.com/spf13/viper"
+	"log"
+)
+
+func Config() *viper.Viper {
+	v := viper.New()
+	//设置读取的配置文件
+	v.SetConfigName("config_resume")
+	//添加读取的配置文件路径
+	v.AddConfigPath("./")
+	if err := v.ReadInConfig(); err != nil {
+		log.Fatalln("配置路径出错", err)
+	}
+	return v
+}
